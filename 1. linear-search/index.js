08e765bcaf86cdb5a83f2c9a152da7e279fe7ba5
Oleh Kozintsev <olehworkshop@gmail.com>
2023-10-09 13:13:27 +0200

    added linear search algorithm

---
+++ 1. linear-search/index.js
@@ -0,0 +1,6 @@
+import {linerSearch} from './linearSearch.js'
+
+const parten = [10, 20, 30, 40, 50, 40, 15, 24]
+
+linerSearch(parten, 24)
+
---
+++ 1. linear-search/linearSearch.js
@@ -0,0 +1,12 @@
+export function linerSearch(array, item) {
+  
+    for(let i=0; i < array.length; i++){
+      
+        if(array[i] === item){       
+        let currentItemIndex = array.indexOf(array[i]) 
+        console.log(currentItemIndex)
+        return
+       }   
+    }
+    console.log('element not finde')
+}
